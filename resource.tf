930c24a1b8e9c81f32843e25bfa57e47f180ea73
Paresh <paresh.birla@gmail.com>
2019-05-14 15:23:47 -0400

    added resource tf file

--- resource.tf
+++ resource.tf
@@ -6,3 +6,10 @@ tags{
 Name = "terraform-aws-vpc-example-two"
 }
 }
+
+resource "aws_subnet" "subnet1" {
+cidr_block = "${cidrsubnet(aws_vpc.enviornment_example-two.cidr_block, 3, 1)}"
+vpc_id = "${aws_vpc.enviornment_example-two.id}"
+availability_zone = "us-east-1a"
+tags { Name = "terraform-aws-subnet"}
+}
